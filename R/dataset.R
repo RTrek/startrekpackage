390769028fe290c2ae2229bbb6fd38d8b83f9703
Roel Hogervorst <hogervorst.rm@gmail.com>
2017-01-20 19:06:06 +0100

    Kinda working version

---
+++ R/dataset.R
@@ -0,0 +1,8 @@
+#' All the utterances of TNG
+#'
+#' Like almost every episode.
+#'
+#' @format a dataframe with every utterance.
+#'
+#' @source \url{blablba}
+"TNGvoices"
---
+++ R/star_trek_package.R
@@ -0,0 +1,16 @@
+#' Star Trek Package
+#'
+#' This package will give you: for every main character
+#' in the Star Trek the Next Generation (TNG) a random
+#' selection of phrases. All the phrases have been
+#' lifted from the original Scripts.
+#'
+#' All functions are called after the Character. So
+#' just call Pulaski() Or Picard().
+#'
+#' This is where you should actually put a link or something
+#' to the original source of the document and the dataset.
+#' @aliases "Star Trek" "TNG package"
+#' @docType package
+#' @name Startrekpackage
+NULL
