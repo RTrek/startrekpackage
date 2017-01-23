54c9127e6af8d7f14a03491e343a442314171e27
Roel Hogervorst <hogervorst.rm@gmail.com>
2017-01-23 17:49:56 +0100

    updated licence

---
+++ vignettes/use_of_this_package.R
@@ -0,0 +1,18 @@
+## ----options-------------------------------------------------------------
+library(startrek)
+Guinan()
+Worf()
+Wesley()
+Picard()
+Beverly()
+Data()
+Geordi()
+Troi()
+Pulaski()
+Q()
+Riker()
+
+## ----other options-------------------------------------------------------
+Computer()
+random_tng_character()
+
